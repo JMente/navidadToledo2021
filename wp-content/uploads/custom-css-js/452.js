a5fc684a6f7dec1e7193db101404786eb241d23a
User01 <user01@iMac01.local>
2021-12-07 15:33:26 -0600

    initial commit

---
+++ wp-content/uploads/custom-css-js/452.js
@@ -0,0 +1,18 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+jQuery(document).ready(function( $ ){
+      const initialHeight = $("#background img").height()
+      $(window).scroll(function () {
+        $('#background img')
+          .css({ 'width': '95vw', 'height': `${initialHeight + $(this).scrollTop()}px` })
+        if ($(window).scrollTop() + $(window).height() == $(document).height())
+          $('#background img')
+            .css({ 'width': '95vw', 'height': `${700}vh` })
+        else
+          $('#background img')
+            .css({ 'width': '95vw', 'height': `${initialHeight + $(this).scrollTop()}px` })
+      });
+});
+
+</script>
+<!-- end Simple Custom CSS and JS -->
